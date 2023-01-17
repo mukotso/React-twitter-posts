95bc5fd360fbb5d4ca3387b0398274bfd4929b6b
mukotso <kelvinmukotso@gmail.com>
2023-01-17 12:03:54 +0300

    edit tweet onclik to update tweet

--- src/Components/EditTweet.js
+++ src/Components/EditTweet.js
@@ -19,39 +19,19 @@ const AddPost = (props) => {
         setTweet(tweet);
         setRemainingTweetCharacters(remainingTweetCharacters)
     }
-
-    // const handleResetTweet = ()=>{
-    //     setTweet('');
-    // }
-
-    const handleSavePost = (props)=>{
-
-        let newTweet =
-            {
-                id: props.tweets.length+1 ,
-                name : "Anonymous User",
-                username: "Anonymous",
-                tweet: tweet
-            }
-
-        let allTweets = props.tweets;
-        allTweets.push(newTweet)
-        props.updateTweet(allTweets);
-        // console.log('tweets',defaultTweets )
-        setTweet('');
-
-        alert('Tweet added successfully');
-
+    const editPostDetails = ()=>{
+        props.handleEditPost(tweet)
     }
+
 
   return(
       <>
-          <button onClick={props.cancelEditTweet} className={'new-tweet-button'} >NEW TWEET</button>git
+          <button onClick={props.cancelEditTweet} className={'new-tweet-button'} >NEW TWEET</button>
         <div className={'addPost'}>
             <textarea ref={tweetInput} onChange={handleTweetChange} className={'text-area'} name={'post'}  value={props.tweet.tweet} placeholder='Type your tweet ....' />
 
         </div>
-          <button onClick={handleSavePost} className={'add-post-button'}>SAVE</button>
+          <button onClick={editPostDetails} className={'add-post-button'}>EDIT</button>
           <button onClick={props.cancelEditTweet} className={'add-post-button'} >CANCEL</button>
           <div className={'wordCount'}>
               <span>{remainingTweetCharacters}</span>
